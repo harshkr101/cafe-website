21f43a7d57fc5b656d4aa7ca704e0803e82a4e78
13lav <lavsaini013@gmail.com>
2021-11-28 15:27:18 +0530

    Refractor Code

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from "react-redux";
+import reduxStore from "./redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={reduxStore}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
+
