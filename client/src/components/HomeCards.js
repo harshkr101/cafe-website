232b61e119077c0bdcea2ba2c519a732f4079fab
13lav <lavsaini013@gmail.com>
2021-11-28 20:52:34 +0530

    Refractor

--- client/src/components/HomeCards.js
+++ client/src/components/HomeCards.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardDeck, Col, Container, Row} from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import CardDeck from "react-bootstrap/Card";
 import card_home_1 from "../assets/card_home_1.jpg";
 import card_home_2 from "../assets/card_home_2.jpg";
 import card_home_3 from "../assets/card_home_3.jpg";
@@ -11,13 +12,13 @@ export default function HomeCards() {
                 <Col>
                     <CardDeck>
                         <Card className='shadow'>
-                            <Card.Img src={card_home_1}/>
+                            <Card.Img src={card_home_1} />
                         </Card>
                         <Card className='shadow'>
-                            <Card.Img src={card_home_2}/>
+                            <Card.Img src={card_home_2} />
                         </Card>
                         <Card className='shadow'>
-                            <Card.Img src={card_home_3}/>
+                            <Card.Img src={card_home_3} />
                         </Card>
                     </CardDeck>
                 </Col>
